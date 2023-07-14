06653f34a8df44d6a03000a2bf781d172fc74080
FERHATSUMER <sumerf39@gmail.com>
2023-07-14 23:27:37 +0300

    codewars

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+// const rps = (p1, p2) => {
+//    if (p1 == 'scissors' && p2 == 'paper') {
+//      return 'Player 1 won!'
+//    }
+//    if (p1 == 'paper' && p2 == 'rock') {
+//      return 'Player 1 won!'
+//    }
+//    if (p1 == 'rock' && p2 == 'scissors') {
+//      return 'Player 1 won!'
+//    }
+//      if (p1 == 'paper' && p2 == 'scissors') {
+//      return 'Player 2 won!'
+//    }
+//    if (p1 == 'rock' && p2 == 'paper') {
+//      return 'Player 2 won!'
+//    }
+//    if (p1 == 'scissors' && p2 == 'rock') {
+//      return 'Player 2 won!'
+//    }
+   
+//    return 'Draw!'
+//  }
+//  rps("paper","rock")
+
 // Find the stray number ->arraydeki farklı sayıyı dönürür
 // function stray(numbers) {
 // return numbers.reduce((acc, number) => acc ^ number);
